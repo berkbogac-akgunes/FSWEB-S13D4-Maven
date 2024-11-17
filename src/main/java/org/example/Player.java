680b41d860c3f76a3bc555965f668dac2eca5cef
berkbogac-akgunes <berkb_akgunes@outlook.com>
2024-11-17 21:26:40 +0300

    project final

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,45 @@
+package org.example;
+
+public class Player {
+    private String name;
+    private int healthPercentage;
+    private Weapon weapon;
+
+    public Player(String name, int healthPercentage, Weapon weapon) {
+        this.name = name;
+        this.healthPercentage = healthPercentage;
+        checkHealth(healthPercentage);
+        this.weapon = weapon;
+    }
+
+    private void checkHealth(int healthPercentage) {
+        if (healthPercentage < 0) {
+            this.healthPercentage = 0;
+        } else if (healthPercentage > 100) {
+            this.healthPercentage = 100;
+        } else {
+            this.healthPercentage = healthPercentage;
+        }
+    }
+
+    public int healthRemaining() {
+        return healthPercentage;
+    }
+
+    public void loseHealth(int damage) {
+        this.healthPercentage = this.healthPercentage - damage;
+
+        if (this.healthPercentage < 0) {
+            this.healthPercentage = 0;
+            System.out.println(name + " player has been knocked out of game");
+        }
+    }
+
+    public void restoreHealth(int healthPotion) {
+        this.healthPercentage = this.healthPercentage + healthPotion;
+
+        if (this.healthPercentage > 100) {
+            this.healthPercentage = 100;
+        }
+    }
+}
